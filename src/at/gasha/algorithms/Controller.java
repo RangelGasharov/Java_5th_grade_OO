4f003d1da429ac1a564dbdb857237abe97786491
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-12-12 12:09:41 +0100

    Added a function for creating random numbers array

--- src/at/gasha/algorithms/Controller.java
+++ src/at/gasha/algorithms/Controller.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Controller {
     private Sorter currentSorter;
     private List<Sorter> availableSorters = new ArrayList<>();
+    private int[] arrayToSort = DataGenerator.generateDataArray(1000, 0, 1000);
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
@@ -18,28 +19,34 @@ public class Controller {
         this.availableSorters.add(insertionSort);
         this.availableSorters.add(selectionSort);
         this.currentSorter = bubbleSort;
-        int[] numbers = DataGenerator.generateDataArray(10000, 0, 100);
         while (isGoing) {
             System.out.println("Enter a number to make the following actions:");
             System.out.println("1 - Choose a sorting algorithm");
             System.out.println("2 - Execute a sorting operation");
-            System.out.println("3 - Add a sorting algorithm");
+            System.out.println("3 - Create random list of length n");
             int selectionNumber = scanner.nextInt();
             switch (selectionNumber) {
                 case 1:
                     changeSorter();
                     break;
                 case 2:
-                    sortingArray(this.currentSorter, numbers);
+                    sortArray(this.currentSorter, this.arrayToSort);
+                    break;
+                case 3:
+                    createRandomArray();
+                    break;
+                default:
+                    System.out.println("The selection is not valid. Please write down a correct number.");
                     break;
             }
         }
     }
 
-    public void sortingArray(Sorter sorter, int[] array) {
+    public void sortArray(Sorter sorter, int[] array) {
         long start = System.currentTimeMillis();
         int[] sortedArray = sorter.sort(array);
         long end = System.currentTimeMillis();
+        DataGenerator.printArray(sortedArray);
         System.out.println(sorter.getName() + ": " + (end - start) + " ms");
     }
 
@@ -50,7 +57,7 @@ public class Controller {
             for (int i = 0; i < this.availableSorters.size(); i++) {
                 System.out.println((i + 1) + ": " + this.availableSorters.get(i).getName());
             }
-            System.out.println("Please choose an algirithm by typing its index.");
+            System.out.println("Please choose an algorithm by typing its index.");
             int selectionNumber = scanner.nextInt();
             if (selectionNumber < 1 || selectionNumber > this.availableSorters.size()) {
                 System.out.println("The given index is not valid. Please try again.");
@@ -63,11 +70,66 @@ public class Controller {
         }
     }
 
+    public void createRandomArray() {
+        boolean isGoing = true;
+        while (isGoing) {
+            System.out.println("You can choose to generate the random array by the following options:");
+            System.out.println("1 - Create a random array of length n");
+            System.out.println("2 - Create a random array of length n with numbers between specified min and max");
+            Scanner scanner = new Scanner(System.in);
+            int selectionNumber = scanner.nextInt();
+            switch (selectionNumber) {
+                case 1:
+                    System.out.println("Please specify the length of the array");
+                    int lengthOfArray = scanner.nextInt();
+                    if (lengthOfArray < 1) {
+                        System.out.println("Please choose a length of at least 1s");
+                    } else {
+                        this.arrayToSort = DataGenerator.generateDataArray(lengthOfArray);
+                        isGoing = false;
+                    }
+                    break;
+                case 2:
+                    System.out.println("Please specify the length of the array");
+                    lengthOfArray = scanner.nextInt();
+                    if (lengthOfArray < 1) {
+                        System.out.println("Please choose a length of at least 1");
+                        break;
+                    }
+
+                    System.out.println("Please specify the minimum value");
+                    int minOfArray = scanner.nextInt();
+
+                    System.out.println("Please specify the maximum value");
+                    int maxOfArray = scanner.nextInt();
+                    if (minOfArray > maxOfArray) {
+                        System.out.println("Maximum has to be greater than or equal minimum valu");
+                        break;
+                    }
+
+                    this.arrayToSort = DataGenerator.generateDataArray(lengthOfArray, minOfArray, maxOfArray);
+                    isGoing = false;
+                    break;
+                default:
+                    System.out.println("Please choose a valid option.");
+                    break;
+            }
+        }
+    }
+
     public Sorter getCurrentSorter() {
         return this.currentSorter;
     }
 
     public void setCurrentSorter(Sorter sorter) {
         this.currentSorter = sorter;
+    }
+
+    public void setArrayToSort(int[] newArray) {
+        this.arrayToSort = newArray;
+    }
+
+    public int[] getArrayToSort() {
+        return this.arrayToSort;
     }
 }
