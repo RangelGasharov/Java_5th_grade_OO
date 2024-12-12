e9bd9087c62676346456bbfe071ef0db8ed2d993
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2024-12-12 11:13:03 +0100

    Added function in the Controller component for changing the current sorter, added object variables, added getter setter

--- src/at/gasha/algorithms/Controller.java
+++ src/at/gasha/algorithms/Controller.java
@@ -5,39 +5,69 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Controller {
-    public static void run() {
+    private Sorter currentSorter;
+    private List<Sorter> availableSorters = new ArrayList<>();
+
+    public void run() {
         Scanner scanner = new Scanner(System.in);
         boolean isGoing = true;
-        DataGenerator generator = new DataGenerator();
-        List<Sorter> availableSorters = new ArrayList<>();
         BubbleSort bubbleSort = new BubbleSort("BubbleSort");
         InsertionSort insertionSort = new InsertionSort("InsertionSort");
         SelectionSort selectionSort = new SelectionSort("SelectionSort");
-        Sorter currentSorter = bubbleSort;
-        availableSorters.add(bubbleSort);
-        availableSorters.add(insertionSort);
-        availableSorters.add(selectionSort);
-        int[] numbers = generator.generateDataArray(10000, 0, 100);
+        this.availableSorters.add(bubbleSort);
+        this.availableSorters.add(insertionSort);
+        this.availableSorters.add(selectionSort);
+        this.currentSorter = bubbleSort;
+        int[] numbers = DataGenerator.generateDataArray(10000, 0, 100);
         while (isGoing) {
             System.out.println("Enter a number to make the following actions:");
             System.out.println("1 - Choose a sorting algorithm");
             System.out.println("2 - Execute a sorting operation");
+            System.out.println("3 - Add a sorting algorithm");
             int selectionNumber = scanner.nextInt();
             switch (selectionNumber) {
                 case 1:
-                    System.out.println("1");
+                    changeSorter();
                     break;
                 case 2:
-                    sortingArray(currentSorter, numbers);
+                    sortingArray(this.currentSorter, numbers);
                     break;
             }
         }
     }
 
-    public static void sortingArray(Sorter sorter, int[] array) {
+    public void sortingArray(Sorter sorter, int[] array) {
         long start = System.currentTimeMillis();
         int[] sortedArray = sorter.sort(array);
         long end = System.currentTimeMillis();
         System.out.println(sorter.getName() + ": " + (end - start) + " ms");
+    }
+
+    public void changeSorter() {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("You can choose between the following sorters:");
+            for (int i = 0; i < this.availableSorters.size(); i++) {
+                System.out.println((i + 1) + ": " + this.availableSorters.get(i).getName());
+            }
+            System.out.println("Please choose an algirithm by typing its index.");
+            int selectionNumber = scanner.nextInt();
+            if (selectionNumber < 1 || selectionNumber > this.availableSorters.size()) {
+                System.out.println("The given index is not valid. Please try again.");
+            } else {
+                setCurrentSorter(this.availableSorters.get(selectionNumber - 1));
+                System.out.println("Algorithm has been changed to Sorter #" + selectionNumber + " -"
+                        + this.currentSorter.getName());
+                break;
+            }
+        }
+    }
+
+    public Sorter getCurrentSorter() {
+        return this.currentSorter;
+    }
+
+    public void setCurrentSorter(Sorter sorter) {
+        this.currentSorter = sorter;
     }
 }
--- src/at/gasha/algorithms/Main.java
+++ src/at/gasha/algorithms/Main.java
@@ -2,22 +2,6 @@ package at.gasha.algorithms;
 
 public class Main {
     public static void main(String[] args) {
-        DataGenerator generator = new DataGenerator();
-        System.out.println("Initial array");
-        int[] numbers = generator.generateDataArray(10, 0, 100);
-        generator.printArray(numbers);
-        System.out.println("Bubblesort array");
-        BubbleSort bubbleSort = new BubbleSort("BubbleSort");
-        int[] numbersBubbleSort = bubbleSort.sort(numbers);
-        generator.printArray(numbersBubbleSort);
-        System.out.println("InsertionSort array");
-        InsertionSort insertionSort = new InsertionSort("InsertionSort");
-        int[] numbersInsertionSort = insertionSort.sort(numbers);
-        generator.printArray(numbersInsertionSort);
-        System.out.println("SelectionSort array");
-        SelectionSort selectionSort = new SelectionSort("SelectionSort");
-        int[] numbersSelectionSort = selectionSort.sort(numbers);
-        generator.printArray(numbersSelectionSort);
         Controller controller = new Controller();
         controller.run();
     }
